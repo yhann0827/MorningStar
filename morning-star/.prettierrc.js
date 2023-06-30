8e4f440ca478ab11792325c6fa79c512b327a226
2EZHenry <henry.choo@2ezasia.com>
2023-06-30 23:09:31 +0800

    chore: setting up project

---
+++ morning-star/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "preserve",
+};
